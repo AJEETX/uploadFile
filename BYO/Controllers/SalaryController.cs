1faa907e72f5ff7e9f91a53272a2233abff9c155
ajeet <ajeet.kumar@f1solutions.com.au>
2019-05-11 18:09:25 +1000

    first commit

---
+++ BYO/Controllers/SalaryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BYO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalaryController : ControllerBase
+    {
+        /// Action to upload file
+        /// </summary>
+        /// <param name="file"></param>
+        [HttpPost]
+        [Route("upload")]
+        public void PostFile(IFormFile file)
+        {
+            var stream = file.OpenReadStream();
+            var name = Path.GetFileName(file.FileName);
+            var JSON = System.IO.File.ReadAllText(name);
+            //TODO: Save file
+        }
+
+    }
+}
---
+++ BYO/Helper/OperationFilter.cs
@@ -0,0 +1,30 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BYO.Helper
+{
+
+    public class FileOperation : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (operation.OperationId.ToLower() == "apifileuploadpost")
+            {
+                operation.Parameters.Clear();//Clearing parameters
+                operation.Parameters.Add(new NonBodyParameter
+                {
+                    Name = "File",
+                    In = "formData",
+                    Description = "Upload Image",
+                    Required = true,
+                    Type = "file"
+                });
+                operation.Consumes.Add("application/form-data");
+            }
+        }
+    }
+}
